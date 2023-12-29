f5ee179e41cf4bc3d541e4e512fa4bb383cc0632
vincelyon <vincentlyondo12@gmail.com>
2023-12-29 10:58:30 +0200

    .

--- BlacWeb/Blac/urls.py
+++ BlacWeb/Blac/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     # Add URLs for admin login and image modification here
     # ...
 ] 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += media(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
