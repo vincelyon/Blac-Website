736d80ecbc8ce1680c4abe88c9943e3805e60e3f
vincelyon <vincentlyondo12@gmail.com>
2023-12-29 11:02:32 +0200

    Update settings.py

--- BlacWeb/BlacWeb/settings.py
+++ BlacWeb/BlacWeb/settings.py
@@ -132,8 +132,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = os.path.join(BASE_DIR,'Blac/static'),
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'Blac/media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
 
