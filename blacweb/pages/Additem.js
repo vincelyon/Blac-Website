863b26be242b7ba87087f7448ee17305b320aa41
vincelyon <vincentlyondo12@gmail.com>
2024-01-04 21:21:43 +0200

    Update Additem.js

--- blacweb/pages/Additem.js
+++ blacweb/pages/Additem.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import db from '../utils/firebaseconfig';
+import { db } from '../utils/firebaseconfig';
 import { collection, getDocs, doc, addDoc } from 'firebase/firestore';
 import withAuth from '../utils/withAuth';
 
