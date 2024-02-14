fc65083e73d637ff9d26be4c6adf8bcddd725c47
vincelyon <vincentlyondo12@gmail.com>
2024-02-14 19:53:07 +0200

    images

--- blacweb/pages/index.js
+++ blacweb/pages/index.js
@@ -99,7 +99,7 @@ const Index = () => {
       <div style={{
         marginTop: '40px',
         height: 'fit-content',
-        backgroundImage: "url('/images/4.jpg')",
+        backgroundImage: "url('/images/fashh.jpg')",
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
         color: 'white'
